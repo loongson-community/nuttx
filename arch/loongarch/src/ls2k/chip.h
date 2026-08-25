2287c2a1cc95ed5eb5312e3c61b2b59b2f666fe1
Ilikara <3435193369@qq.com>
2026-08-25 17:56:05 +0800

    arch/loongarch: add ls2k SoC build integration
    Add the LS2K0300 chip selection and the SoC-level build integration:
    the ls2k Kconfig fragment tree, Make.defs, the SoC hardware
    configuration header and the heap allocation glue, together with the
    LS2K0300 chip selection wired into arch/loongarch/Kconfig.
    
    Signed-off-by: maoxiaochuan <maoxiaochuan@loongson.cn>
    Signed-off-by: ilikara <3435193369@qq.com>

---
+++ arch/loongarch/src/ls2k/chip.h
@@ -0,0 +1,52 @@
+/****************************************************************************
+ * arch/loongarch/src/ls2k/chip.h
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __ARCH_LOONGARCH_SRC_LS2K_CHIP_H
+#define __ARCH_LOONGARCH_SRC_LS2K_CHIP_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+/* Include the chip capabilities file */
+
+#include <arch/ls2k/chip.h>
+
+/* Include the chip interrupt definition file */
+
+#include <arch/ls2k/irq.h>
+
+/* Include the chip memory map */
+
+#include "hardware/ls_memorymap.h"
+
+/* Include the chip pinmap */
+
+#include "hardware/ls_pinmap.h"
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+#endif /* __ARCH_LOONGARCH_SRC_LS2K_CHIP_H */
