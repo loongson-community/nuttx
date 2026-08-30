023ad357bcaf238d2ccfd890c58f86547ec4eceb
Ilikara <3435193369@qq.com>
2026-08-30 14:28:01 +0800

    arch/loongarch: add LS2K SPIIO support
    Add the LS2K SPI IO controller driver.
    
    Signed-off-by: maoxiaochuan <maoxiaochuan@loongson.cn>
    Signed-off-by: ilikara <3435193369@qq.com>

--- boards/loongarch/ls2k/hummingbird-ls2k0300/src/hummingbird-ls2k0300.h
+++ boards/loongarch/ls2k/hummingbird-ls2k0300/src/hummingbird-ls2k0300.h
@@ -96,5 +96,9 @@ extern int ls_pwm_initialize(int port);
 extern struct spi_dev_s *ls_spiflash_initialize(int port);
 #endif
 
+#ifdef CONFIG_LS_SPIIO
+extern struct spi_dev_s *ls_spiio_initialize(int port);
+#endif
+
 #endif /* __ASSEMBLY__ */
 #endif /* __BOARDS_LOONGARCH_LS2K_HUMMINGBIRD_LS2K0300_SRC_HUMMINGBIRD_LS2K0300_H */
